aa317c659c4019c443f051781959109628cbc376
Merry McCarron <mmccarron@gmail.com>
2015-08-06 13:38:35 -0700

    more queries to make our master tables

--- hive/hivequeries.sql
+++ hive/hivequeries.sql
@@ -50,10 +50,10 @@ FROM bills_count_lob bills
 LEFT JOIN lob_financials_internal fin on (bills.lcl_id = fin.lcl_id);
 
 --this generates the idea behind this whole exercise: amt spent per bill
-select lower(bill_norm), sum(per_bill_fund) as total_spent
+select lower(bill_norm), bill_period, sum(per_bill_fund) as total_spent
 from bills_lobbyists_internal bills 
 left join bills_count_comp comp ON bills.lcl_id = comp.lcl_id
-group by bill_norm
+group by bill_norm, bill_period
 limit 20;
 
 --creates a table of all bills that have been lobbied on with the total spent on them
@@ -61,7 +61,11 @@ CREATE TABLE total_spent_bills
 row format delimited fields terminated by ','
 STORED AS RCFile
 AS
-select lower(bill_norm), bill_period, sum(per_bill_fund) as total_spent
+select 
+  lower(regexp_replace(bill_norm, '"', '')) as bill,
+  regexp_replace(bill_period, '"', '')as year,
+  sum(per_bill_fund) as total_spent,
+  avg(per_bill_fund) as avg_spent_bill
 from bills_lobbyists_internal bills 
 left join bills_count_comp comp ON bills.lcl_id = comp.lcl_id
 group by bill_norm, bill_period;
@@ -69,7 +73,7 @@ group by bill_norm, bill_period;
 --count number of bills for word
 create external table words_bills (word string, bill string, year string)
 row format delimited fields terminated by '\t'
-location '/user/cloudera/billsHive/wordsBills';
+location '/user/cloudera/billsHive/wordstobills';
 
 CREATE TABLE words_bills_count 
 row format delimited fields terminated by ','
@@ -79,3 +83,41 @@ SELECT word, count(*) num_of_bills
 FROM words_bills
 GROUP BY word
 ;
+
+create external table tfidf (word string, tfidf float)
+row format delimited fields terminated by '\t'
+location '/user/cloudera/billsHive/tfidf';
+
+--aggregate table with both bills cound and tfidf score per word.
+CREATE TABLE words_bills_count_tfidf
+row format delimited fields terminated by ','
+STORED AS RCFile
+AS
+SELECT tfidf.word, num_of_bills, tfidf
+FROM tfidf
+LEFT JOIN words_bills_count on (tfidf.word = words_bills_count.word)
+;
+
+
+CREATE TABLE spend_per_word
+row format delimited fields terminated by ','
+STORED as RCFile
+AS
+select 
+  words.word, 
+  round(sum(spent.total_spent),2) as total_spent_word, 
+  round(avg(spent.avg_spent_bill),2) as avg_spent_word
+from words_bills words
+left join total_spent_bills spent on (words.bill = spent.bill and words.year = spent.year)
+group by words.word
+;
+
+CREATE TABLE master_word_metrics
+row format delimited fields terminated by ','
+STORED as RCFile
+AS
+select tf.word, num_of_bills, tf.tfidf, spend.total_spent_word, spend.avg_spent_word
+from words_bills_count_tfidf tf
+left join spend_per_word spend on (tf.word = spend.word)
+order by total_spent_word desc
+limit 25;
